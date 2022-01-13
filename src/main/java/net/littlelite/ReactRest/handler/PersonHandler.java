978de30a4056711b43fccd9bdd58f32ebec6aecd
Alessio Saltarin <alessiosaltarin@gmail.com>
2022-01-13 12:30:58 +0100

    Added OpenAPI docs

--- src/main/java/net/littlelite/ReactRest/handler/PersonHandler.java
+++ src/main/java/net/littlelite/ReactRest/handler/PersonHandler.java
@@ -12,10 +12,8 @@ import net.littlelite.ReactRest.repository.PersonRepository;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Component
--- src/main/java/net/littlelite/ReactRest/router/rest/RouterRest.java
+++ src/main/java/net/littlelite/ReactRest/router/rest/RouterRest.java
@@ -8,6 +8,8 @@ package net.littlelite.ReactRest.router.rest;
 
 import net.littlelite.ReactRest.handler.PersonHandler;
 import org.jetbrains.annotations.NotNull;
+import org.springdoc.core.annotations.RouterOperation;
+import org.springdoc.core.annotations.RouterOperations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
@@ -15,18 +17,23 @@ import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
 
 @Component
 public class RouterRest
 {
     @Bean
+    @RouterOperations({ @RouterOperation(path = "/api/persons/{id}", beanClass = PersonHandler.class, beanMethod = "getPersonById"),
+            @RouterOperation(path = "/api/persons/age/{age}", beanClass = PersonHandler.class, beanMethod = "getPersonsByAge"),
+            @RouterOperation(path = "/api/persons", beanClass = PersonHandler.class, beanMethod = "getAllPersons") })
     public RouterFunction<ServerResponse> root(@NotNull PersonHandler personHandler)
     {
-        return RouterFunctions.route()
-                .GET("/api/persons/{id}", accept(MediaType.TEXT_PLAIN), personHandler::getPersonById)
-                .GET("/api/persons/age/{age}", accept(MediaType.TEXT_PLAIN), personHandler::getPersonsByAge)
-                .GET("/api/persons", accept(MediaType.TEXT_PLAIN), personHandler::getAllPersons)
-                .build();
+        return RouterFunctions
+                .route(GET("/api/persons/{id}").and(accept(MediaType.APPLICATION_JSON)), personHandler::getPersonById)
+                .andRoute(GET("/api/persons/age/{age}").and(accept(MediaType.APPLICATION_JSON)), personHandler::getPersonsByAge)
+                .andRoute(GET("/api/persons").and(accept(MediaType.APPLICATION_JSON)), personHandler::getAllPersons);
+
     }
+
 }
